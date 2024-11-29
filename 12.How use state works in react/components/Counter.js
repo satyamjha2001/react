0dfc61bb2d5f5242f670917d8c769b89a20e34af
satyamjha2001 <satyamjha23112001@gmail.com>
2024-11-29 17:51:04 +0530

    added how use states works its behind the scenes.

---
+++ 12.How use state works in react/components/Counter.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Counter() {
+  // use state makes array and changes the value of states array when we use setState.
+  // const states = [3, "Satyam Kumar Jha"];
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Satyam Jha");
+  console.log("rendering");
+  
+  
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>{count}</h1>
+      <h2>{name}</h2>
+      <button
+        onClick={() => {
+          // so if we write setCount more than 1 times then also it reflect change 1 time.
+          // setCount(count + 1);
+          // setCount(count + 1);
+          // setCount(count + 1);
+          // for solving this if we use the previous state then we have to give callback function.
+          setCount(function (prevCount) {
+            return prevCount + 1;
+          });
+          setCount((prevCount) => prevCount + 1);
+          setCount((prevCount) => prevCount + 1);
+          setName("Satyam Kumar Jha");
+          console.log(count);
+        }}
+      >
+        Increase count
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
