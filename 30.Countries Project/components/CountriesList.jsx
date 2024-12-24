7860a01c595eefd334cae12a8bc06317464915c4
satyamjha2001 <satyamjha23112001@gmail.com>
2024-12-25 01:33:27 +0530

    Countries Project Completed

---
+++ 30.Countries Project/components/CountriesList.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard";
+import CountriesListShimmer from "./CountriesListShimmer";
+
+export default function CountriesList({ query }) {
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountriesData(data);
+      });
+  }, []);
+
+  if (countriesData.length === 0) {
+    return <CountriesListShimmer />;
+  }
+
+  return (
+    <>
+      <div className="countries-container">
+        {countriesData
+          .filter((country) => {
+            return country.name.common.toLowerCase().includes(query) || country.region.toLowerCase().includes(query);
+          })
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population.toLocaleString("en-IN")}
+                region={country.region}
+                capital={country.capital?.[0]}
+                country={country}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
+}
---
+++ 30.Countries Project/components/CountryCard.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router";
+
+export default function CountryCard({
+  name,
+  flag,
+  population,
+  region,
+  capital,
+  country,
+}) {
+  return (
+    <Link className="country-card" to={`/${name}`} state={country}>
+      <div className="flag-container">
+      <img src={flag} alt={`${name} flag`} />
+      </div>
+      <div className="card-text">
+        <h3 className="card-title">{name}</h3>
+        <p>
+          <b>Population: </b>
+          {population}
+        </p>
+        <p>
+          <b>Region: </b>
+          {region}
+        </p>
+        <p>
+          <b>Capital: </b>
+          {capital}
+        </p>
+      </div>
+    </Link>
+  );
+}
